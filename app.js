9c560390bd78fe2eb3af2d09b73182808305faee
Boccacio <bucharnechari@gmail.com>
2022-11-11 18:36:23 +0300

    mongodb ayarlamarı yapıldı

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var createError = require('http-errors');
+require("./app_server/models/db");
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+
+
 
 var app = express();
 
